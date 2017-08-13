2b49bf1e9ec3fc69ea71013493357cafd778487c
Mathieu Lue <m.lue@rowan.nyc>
2017-08-13 16:02:07 -0400

    ok

--- echo.js
+++ echo.js
@@ -38,8 +38,8 @@ bot.on('message', function(message) {
     setTimeout(() => {
       // message.reply(reactions[Math.floor(Math.random() * reactions.length)]).catch((e) => console.log(e))
       message.react(reactions[Math.floor(Math.random() * reactions.length)]).catch((e) => console.log(e))
-      var critiques = [`I'm not sure about ${pro}`, `${pro} was definitely unfair to ${sup}`, `Should ${pro} end up happy? ${sup} was a shit`, `Should ${pro} end up happy? ${sup} was a shit`, `Should ${pro} end up happy? ${sup} was a shit`]
-      message.reply(critiques[Math.floor(Math.random() * critiques.length)]).catch((e) => console.log(e))
+      var critiques = [`I'm not sure about ${pro}`, `${pro} was definitely unfair to ${sup}`, `Should ${pro} end up happy? What about ${sup}?`, `Should ${pro} end up happy? ${sup} was a shit`, `I don't understand ${pro}`, `${pro} didn't deserve that`, `This story makes no sense to me`, `Is this nonsense?`, `I guess the takeaway is that, in life people like ${pro} take advantage of people like ${sup}`]
+      if(message.content.length > 1500)message.reply(critiques[Math.floor(Math.random() * critiques.length)]).catch((e) => console.log(e))
       message.channel.stopTyping();
     }), 5000
   }
