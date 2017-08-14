2c5a8c16d73a617bef98f0835a1b3915fea0f551
Mathieu Lue <mlue@primary.com>
2017-08-14 16:04:02 -0400

    SAVEPOINT

--- dist/bot.js
+++ dist/bot.js
@@ -474,7 +474,7 @@ class Bot {
     this.discord.on('message', message => {
       if(message.content.match(/.{5,}\..{10,}/))megahal.addMass(message.content)
       else megahal.add(message.content)
-      if(message.author.id != bot.id && message.channel.id == '345940851412828161' && message.author.username == 'echo')setTimeout(() => {
+      if(message.author.id != this.id && message.channel.id == '345940851412828161' && message.author.username == 'echo')setTimeout(() => {
           message.channel.send(megahal.getReplyFromSentence(message.content))
       }, Math.random() * 20000)
       if(message.content.match(/^gimme a script/) && message.author.username != 'gbp'){
