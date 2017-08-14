991cf394e0637fc64194c4b5f284f4cec0f77995
Mathieu Lue <mathieu.lue@gmail.com>
2017-08-13 23:43:18 -0400

    SAVEPOINT

--- echo.js
+++ echo.js
@@ -39,7 +39,7 @@ function askForScript() {
 function startStream(s){
   client.stream('statuses/filter', s,  function(stream) {
     stream.on('data', function(tweet) {
-      if(Math.random() > 0.5){
+      if(Math.random() > 0){
         var embed = new Discord.RichEmbed()
             .setColor("#"+tweet.user.profile_background_color)
             .setTimestamp(new Date(tweet.created_at))
@@ -48,7 +48,7 @@ function startStream(s){
             .setTitle(tweet.text)
             .addField("retweets", tweet.retweet_count, true)
             .setFooter(tweet.user.screen_name)
-            .setURL(`https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`)
+           .setURL(`https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`)
         if(tweet.user.location)embed.addField("location", tweet.user.location, true)
         bot.channels.find( c => c.id == '345940851412828161').send({embed}).catch(e => console.log(e));
       }
@@ -59,12 +59,12 @@ function startStream(s){
 
 bot.on('message', function(message) {
   var delay = Math.random()* 30000
-  if(message.author.id != bot.id && message.channel.id == '345940851412828161' && message.author.username == 'gbp' && message.content.match(/~fin~/)){
-    if(message.content.length > 1000)message.channel.startTyping()
+  if(message.author.id != bot.id && message.channel.id == '345940851412828161' && message.author.username == 'gbp')){
+    if(message.content.length > 800)message.channel.startTyping()
     var topics = _l(nlp(message.content).nouns().out('array')).countBy().toPairs().sortBy(e => -e[1]).value();
     var pro = topics[0][0]
     var sup = topics[1][0]
-    if(message.content.length > 1000)setTimeout(() => {
+    if(message.content.length > 800)setTimeout(() => {
       // message.reply(reactions[Math.floor(Math.random() * reactions.length)]).catch((e) => console.log(e))
       message.react(reactions[Math.floor(Math.random() * reactions.length)]).catch((e) => console.log(e))
       var critiques = [`I'm not sure about ${pro}`, `${pro} was definitely unfair to ${sup}`, `Should ${pro} end up happy? What about ${sup}?`, `Should ${pro} end up happy? ${sup} was a shit`, `I don't understand ${pro}`, `${pro} didn't deserve that`, `This story makes no sense to me`, `Is this nonsense?`, `I guess the takeaway is that, in life people like ${pro} take advantage of people like ${sup}`, `What does what happened to ${pro} say about anything?`, `What could ${pro} represent in relation to ${sup}`]
@@ -75,4 +75,4 @@ bot.on('message', function(message) {
   // if(resp && userID != bot.id){console.log('going to respond to resp in %s seconds',delay/1000), setTimeout(() => {
   // }, delay)}
 })
-bot.login(process.env.SECRET).then(() => {startStream({track: 'artificialintelligence, mvci, cryptocurrency, ethereum, openai, skynet', language: 'en', filter_level: 'medium'}); askForScript(); emojis = bot.guilds.first().emojis})
+bot.login(process.env.SECRET).then(() => {startStream({track: 'artificialintelligence, mvci, cryptocurrency, ethereum, openai, skynet', language: 'en', filter_level: 'low'}); askForScript(); emojis = bot.guilds.first().emojis})
